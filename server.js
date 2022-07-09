3c064a86eeb5ccfa10994c4372477074d59fc425
Chris(PC) <bissuetox@gmail.com>
2022-07-08 18:16:50 -0700

    ejs templating integration

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = 80;
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/art', (req, res) => {
+  res.render('art');
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.use((req,res) => {
+  res.status(404).render('404');
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 
